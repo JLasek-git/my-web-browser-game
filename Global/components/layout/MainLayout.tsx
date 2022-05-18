0f1c9715a8a2a9ce6512878f482a94a263c56a63
Jakub <jakublasek41@gmail.com>
2022-05-18 14:47:46 +0200

    Small changes in main-layout

--- Global/components/layout/MainLayout.tsx
+++ Global/components/layout/MainLayout.tsx
@@ -5,9 +5,9 @@ type Props = {
 };
 const MainLayout = (props: Props) => {
   return (
-    <main className="app__container w-screen h-screen flex justify-center bg-zinc-700">
+    <main className="app__container w-screen h-screen flex justify-center p-2 bg-zinc-700">
       <NavigationDrawer />
-      <section className="h-full w-full p-4">{props.children}</section>
+      <section className="h-full w-full">{props.children}</section>
     </main>
   );
 };
--- Global/components/layout/NavigationDrawer.tsx
+++ Global/components/layout/NavigationDrawer.tsx
@@ -5,7 +5,7 @@ import { availableAppRoutes } from "../../../App/routes/app.routes";
 type Props = {};
 const NavigationDrawer = (props: Props) => {
   return (
-    <nav className="w-80 h-full bg-zinc-700 p-4 text-white">
+    <nav className="w-96 h-full bg-zinc-700 text-white">
       <div className="h-full w-full bg-zinc-800">
         <UserProfileInfo />
 
