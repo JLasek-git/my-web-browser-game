39eb476370b9f47d07b070719df187b344c111e3
Jakub <jakublasek41@gmail.com>
2022-05-18 13:49:04 +0200

    Add tailwind config and basic cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./App/components/**/**/*.{js,ts,jsx,tsx}",
+    "./Global/components/**/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
