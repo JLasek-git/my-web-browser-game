fb96e0e76b029ec93b411d2b089a4526ee30eafa
Jakub <jakublasek41@gmail.com>
2022-05-18 14:40:10 +0200

    Add active link styles

--- Global/components/layout/NavigationItem.tsx
+++ Global/components/layout/NavigationItem.tsx
@@ -1,14 +1,32 @@
 import { Route } from "../../../App/routes/app.routes";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { AppRoutesEnum } from "../../../App/enums/AppRoutesEnum";
 
 type Props = {
   route: Route;
 };
 
 const NavigationItem = (props: Props) => {
+  const router = useRouter();
+
   return (
-    <div className="w-full h-fit flex flex-col justify-start p-4 hover:bg-zinc-900">
-      <Link href={props.route.path}>{props.route.name}</Link>
+    <div
+      className={
+        "w-full h-fit flex flex-col justify-start hover:bg-zinc-900 cursor-pointer"
+      }
+    >
+      <Link href={props.route.path}>
+        <span
+          className={
+            router.pathname === props.route.path
+              ? "p-4 bg-zinc-900 font-bold"
+              : "p-4"
+          }
+        >
+          {props.route.name}
+        </span>
+      </Link>
     </div>
   );
 };
